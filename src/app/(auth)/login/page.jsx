600058abca4f6c68988ac3fd7c74a942b3947530
raju2277 <yajuddinraju1@gmail.com>
2026-04-30 09:07:43 -0700

    register form setup

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,24 +1,35 @@
+"use client";
+
 import Image from 'next/image';
 import React from 'react';
 import img from '@/assets/loginLogo.png'
 import { FcGoogle } from 'react-icons/fc';
 import Link from 'next/link';
+import { useForm } from 'react-hook-form';
+
+
+
 
 const LoginPage = () => {
+
+    const {register,handleSubmit}=useForm();
+    const handleLoginFunction=(data)=>{
+        console.log(data)
+    }
     return (
         <div className=' min-h-screen flex items-center justify-center rounded-xl'>
             {/* login section */}
             <div className="bg-gray-100 flex shadow-lg p-5">
                 {/* login form */}
                 <div className="sm:w-1/2 flex items-center">
-                    <fieldset className="fieldset border-base-100 rounded-box w-xs border p-4">
+                    <form onSubmit={handleSubmit(handleLoginFunction)} className="fieldset border-base-100 rounded-box w-xs border p-4">
                         <legend className="fieldset-legend text-2xl font-bold text-purple-500">Login</legend>
 
                         <label className="label">Email</label>
-                        <input type="email" className="input rounded-xl" placeholder="Email" />
+                        <input type="email" {...register("email")} className="input rounded-xl" placeholder="Email" />
 
                         <label className="label">Password</label>
-                        <input type="password" className="input rounded-xl" placeholder="Password" />
+                        <input type="password" {...register("password",{required:true})} className="input rounded-xl" placeholder="Password" />
 
                         <button className="btn bg-purple-500 text-white mt-4">Login</button>
 
@@ -28,7 +39,7 @@ const LoginPage = () => {
 
                           <p className='text-center'>Don't have an account? <Link className='text-purple-500' href={"/register"}>Register</Link></p>
 
-                    </fieldset>
+                    </form>
                 </div>
                 {/* image */}
                 <div className="w-1/2 p-5 bg-purple-50 rounded-lg sm:block hidden ">
--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,11 +1,60 @@
-import React from 'react';
+"use client";
 
-const RegisterPage = () => {
+import Image from 'next/image';
+import React from 'react';
+import img from '@/assets/loginLogo.png'
+import { FcGoogle } from 'react-icons/fc';
+import Link from 'next/link';
+import { useForm } from 'react-hook-form';
+
+
+
+
+const LoginPage = () => {
+
+    const {register,handleSubmit}=useForm();
+    const handleRegFunction=(data)=>{
+        console.log(data)
+    }
     return (
-        <div>
-            register
+        <div className=' min-h-screen flex items-center justify-center rounded-xl'>
+            {/* login section */}
+            <div className="bg-gray-100 flex shadow-lg p-5">
+                {/* login form */}
+                <div className="sm:w-1/2 flex items-center">
+                    <form onSubmit={handleSubmit(handleRegFunction)} className="fieldset border-base-100 rounded-box w-xs border p-4">
+                        <legend className="fieldset-legend text-2xl font-bold text-purple-500">Register</legend>
+
+                        <label className="label">Name</label>
+                        <input type="text" {...register("name")} className="input rounded-xl" placeholder="Name" />
+
+                        <label className="label">Photo Url</label>
+                        <input type="text" {...register("photo")} className="input rounded-xl" placeholder="Url" />
+
+                        <label className="label">Email</label>
+                        <input type="email" {...register("email")} className="input rounded-xl" placeholder="Email" />
+
+                        <label className="label">Password</label>
+                        <input type="password" {...register("password",{required:true})} className="input rounded-xl" placeholder="Password" />
+
+                        <button className="btn bg-purple-500 text-white mt-4">Register</button>
+
+                          <div className="divider">OR</div>
+
+                          
+
+                          <p className='text-center'> Have an account? <Link className='text-purple-500' href={"/login"}>Login</Link></p>
+
+                    </form>
+                </div>
+                {/* image */}
+                <div className="w-1/2 p-5 bg-purple-50 rounded-lg sm:block hidden ">
+                    <Image className='rounded-2xl' src={img} height={600} width={600} alt='logo'></Image>
+                </div>
+            </div>
+
         </div>
     );
 };
 
-export default RegisterPage;
+export default LoginPage;
